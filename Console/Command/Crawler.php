fd5c8a3c1e9b0b63e7f397897fefcf709f1ce4ae
Andrew Kett <andrew@8wiredigital.co.nz>
2019-02-18 22:30:39 +1300

    add ability to pass console options into crawler

--- Console/Command/Crawler.php
+++ Console/Command/Crawler.php
@@ -5,6 +5,7 @@ namespace EightWire\Primer\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 class Crawler extends Command
 {
@@ -31,7 +32,13 @@ class Crawler extends Command
     protected function configure()
     {
         $this->setName('primer:crawler:run')
-            ->setDescription('Remove invalid products based on a given search strategy');
+            ->addOption('batch-size', null, InputOption::VALUE_OPTIONAL, 'Max number of pages to crawl per batch')
+            ->addOption('max-run-time', null, InputOption::VALUE_OPTIONAL, 'Max time in seconds for the crawler to run before exiting')
+            ->addOption('sleep-between-batch', null, InputOption::VALUE_OPTIONAL, 'Time in seconds to wait between batches')
+            ->addOption('sleep-when-empty', null, InputOption::VALUE_OPTIONAL, 'Time in seconds to wait before trying again when no pages were crawled')
+            ->addOption('crawl-threshold', null, InputOption::VALUE_OPTIONAL, 'Minimum priority for logged page to reach before being crawled')
+            ->addOption('when-complete', null, InputOption::VALUE_OPTIONAL, 'What to do when all pages have been logged (sleep or stop)')
+            ->setDescription('Initiate primer crawler');
     }
 
     /**
@@ -45,13 +52,37 @@ class Crawler extends Command
     {
         $this->crawler->setOutput($output);
 
-        //@todo console params to set crawler config. e.g
-//        $this->crawler->setWhenComplete(\EightWire\Primer\Model\Crawler::WHEN_COMPLETE_STOP);
-//        $this->crawler->setSleepBetweenBatch(10);
-//        $this->crawler->setSleepWhenEmpty(20);
-//        $this->crawler->setBatchSize(5);
-//        $this->crawler->setMaxRunTime(20);
-//        $this->crawler->setCrawlThreshold(3);
+
+        $batchSize = $input->getOption('batch-size');
+        if ($batchSize) {
+            $this->crawler->setBatchSize($batchSize);
+        }
+
+        $maxRunTime = $input->getOption('max-run-time');
+        if ($maxRunTime) {
+            $this->crawler->setMaxRunTime($maxRunTime);
+        }
+
+        $sleepBetweenBatch = $input->getOption('sleep-between-batch');
+        if ($sleepBetweenBatch) {
+            $this->crawler->setSleepBetweenBatch($sleepBetweenBatch);
+        }
+
+        $sleepWhenEmpty = $input->getOption('sleep-when-empty');
+        if ($sleepWhenEmpty) {
+            $this->crawler->setSleepWhenEmpty($sleepWhenEmpty);
+        }
+
+        $crawlThreshold = $input->getOption('crawl-threshold');
+        if ($crawlThreshold) {
+            $this->crawler->setCrawlThreshold($crawlThreshold);
+        }
+
+        $whenComplete= $input->getOption('when-complete');
+        if ($whenComplete) {
+            $this->crawler->setWhenComplete($whenComplete);
+        }
+
         $this->crawler->run();
     }
 }
